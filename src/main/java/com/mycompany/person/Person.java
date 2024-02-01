3c556787baeddd4719fc9323b7becab4179a8e63
Yafigg <cipto.wongso88@gmail.com>
2024-02-01 08:45:41 +0700

    Pertama

---
+++ src/main/java/com/mycompany/person/Person.java
@@ -0,0 +1,16 @@
+package com.mycompany.person;
+
+class Person {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    
+
+}
---
+++ src/main/java/com/mycompany/person/main.java
@@ -0,0 +1,11 @@
+package com.mycompany.person;
+
+public class main {
+    public static void main(String[] args) {
+        Person mPerson = new Person();
+        mPerson.setName("Yafigg Ganteng ");
+        mPerson.getName();
+
+        System.out.println("Person Name: " + mPerson.getName());
+    }
+}
